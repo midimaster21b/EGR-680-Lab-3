1e4183e5fa9b6a8cd6f232fd96c40977bdb52da3
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-09-15 19:18:08 -0400

    Define states for Moore state machine

--- Lab3.srcs/sources_1/new/vending_machine.v
+++ Lab3.srcs/sources_1/new/vending_machine.v
@@ -20,6 +20,28 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+// Define states
+`define reset              4'b0000;
+
+// Only money input
+`define five_cents         4'b0001;
+`define ten_cents          4'b0010;
+`define fifteen_cents      4'b0011;
+`define twenty_cents       4'b0100;
+
+// 10 cents dispense states
+`define dispense_gum_10    4'b0101;
+
+// 15 cents dispense states
+`define dispense_gum_15    4'b0110;
+`define dispense_apple_15  4'b0110;
+
+// 20 cents dispense states
+`define dispense_gum_20    4'b0111;
+`define dispense_apple_20  4'b1000;
+`define dispense_yogurt_20 4'b1001;
+
+
 module vending_machine(
     input clk,
     input rst,
